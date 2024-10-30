2c47815839b2482b85f17c3806573e90fa154749
airoca <jyh0872@gmail.com>
2024-10-31 01:45:18 +0900

    3주차 기본 구현

--- src/main/java/diary/dto/DiaryRequest.java
+++ src/main/java/diary/dto/DiaryRequest.java
@@ -2,16 +2,19 @@ package diary.dto;
 
 import diary.repository.DiaryEntity;
 
-public record DiaryRequest(String title, String content, DiaryEntity.Category category) {
+public record DiaryRequest(String title, String content, DiaryEntity.Category category, Boolean isVisible) {
     public void validate() {
-        if (title == null) {
+        if (title == null || title.isEmpty()) {
             throw new IllegalArgumentException("제목을 입력해주세요.");
         }
-        if (content == null) {
+        if (content == null || content.isEmpty()) {
             throw new IllegalArgumentException("내용을 입력해주세요.");
         }
         if (category == null) {
             throw new IllegalArgumentException("카테고리를 입력해주세요.");
+        }
+        if (isVisible == null) {
+            throw new IllegalArgumentException("공개 여부를 설정해주세요.");
         }
     }
 }
