75531b4c7fc0e99da77a3c9e9b820fe01cd2127e
airoca <jyh0872@gmail.com>
2024-11-05 19:12:00 +0900

    feat: API 엔드포인트 정리

--- src/main/java/diary/controller/DiaryController.java
+++ src/main/java/diary/controller/DiaryController.java
@@ -9,94 +9,95 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/diaries")
 public class DiaryController {
     private final DiaryService diaryService;
 
-    public DiaryController(DiaryService diaryService){
+    public DiaryController(DiaryService diaryService) {
         this.diaryService = diaryService;
     }
 
-    @PostMapping("/api/diary")
-    void post(@RequestBody DiaryRequest diaryRequest, @RequestHeader("Authorization") Long token){
+    @PostMapping
+    void post(@RequestBody DiaryRequest diaryRequest, @RequestHeader("Authorization") Long token) {
         diaryRequest.validate();
         diaryService.createDiary(token, diaryRequest.title(), diaryRequest.content(), diaryRequest.category(), diaryRequest.isVisible());
     }
 
-    @GetMapping("/api/diary/all")
-    ResponseEntity<DiaryListResponse> get(@RequestHeader("Authorization") Long token){
+    @GetMapping("/all")
+    ResponseEntity<DiaryListResponse> get(@RequestHeader("Authorization") Long token) {
         List<Diary> diaryList = diaryService.getAllDiary();
 
         List<DiaryResponse> diaryResponseList = new ArrayList<>();
-        for(Diary diary : diaryList){
-            diaryResponseList.add(new DiaryResponse(diary.id(), diary.title() ,diary.date(), diary.category(), diary.isVisible(), diary.userId(), diary.nickname()));
+        for (Diary diary : diaryList) {
+            diaryResponseList.add(new DiaryResponse(diary.id(), diary.title(), diary.date(), diary.category(), diary.isVisible(), diary.userId(), diary.nickname()));
         }
 
         return ResponseEntity.ok(new DiaryListResponse(diaryResponseList));
     }
 
-    @GetMapping("/api/diary")
-    ResponseEntity<DiaryListResponse> getRecent(@RequestHeader("Authorization") Long token){
+    @GetMapping
+    ResponseEntity<DiaryListResponse> getRecent(@RequestHeader("Authorization") Long token) {
         List<Diary> diaryList = diaryService.getRecentDiary();
 
         List<DiaryResponse> diaryResponseList = new ArrayList<>();
-        for(Diary diary : diaryList){
+        for (Diary diary : diaryList) {
             diaryResponseList.add(new DiaryResponse(diary.id(), diary.title(), diary.date(), diary.category(), diary.isVisible(), diary.userId(), diary.nickname()));
         }
 
         return ResponseEntity.ok(new DiaryListResponse(diaryResponseList));
     }
 
-    @GetMapping("/api/diary/sorted")
-    ResponseEntity<DiaryListResponse> getSorted(@RequestHeader("Authorization") Long token){
+    @GetMapping("/sorted")
+    ResponseEntity<DiaryListResponse> getSorted(@RequestHeader("Authorization") Long token) {
         List<Diary> diaryList = diaryService.getSortedDiary();
 
         List<DiaryResponse> diaryResponseList = new ArrayList<>();
-        for (Diary diary : diaryList){
+        for (Diary diary : diaryList) {
             diaryResponseList.add(new DiaryResponse(diary.id(), diary.title(), diary.date(), diary.category(), diary.isVisible(), diary.userId(), diary.nickname()));
         }
 
         return ResponseEntity.ok(new DiaryListResponse(diaryResponseList));
     }
 
-    @GetMapping("/api/diary/{id}")
+    @GetMapping("/{id}")
     ResponseEntity<DiaryDetailResponse> getById(@PathVariable Long id, @RequestHeader("Authorization") Long token) {
         Diary diary = diaryService.getDiaryById(id);
 
         return ResponseEntity.ok(new DiaryDetailResponse(diary.id(), diary.title(), diary.content(), diary.date(), diary.category(), diary.isVisible(), diary.userId(), diary.nickname()));
     }
 
-    @GetMapping("/api/diary/category/{category}")
-    ResponseEntity<DiaryListResponse> getByCategory(@PathVariable DiaryEntity.Category category, @RequestHeader("Authorization") Long token){
+    @GetMapping("/category/{category}")
+    ResponseEntity<DiaryListResponse> getByCategory(@PathVariable DiaryEntity.Category category, @RequestHeader("Authorization") Long token) {
         List<Diary> diaryList = diaryService.getDiariesByCategory(category);
 
         List<DiaryResponse> diaryResponseList = new ArrayList<>();
-        for (Diary diary : diaryList){
+        for (Diary diary : diaryList) {
             diaryResponseList.add(new DiaryResponse(diary.id(), diary.title(), diary.date(), diary.category(), diary.isVisible(), diary.userId(), diary.nickname()));
         }
 
         return ResponseEntity.ok(new DiaryListResponse(diaryResponseList));
     }
 
-    @GetMapping("/api/diary/user")
-    ResponseEntity<DiaryListResponse> getByUserId(@RequestHeader("Authorization") Long token){
+    @GetMapping("/user")
+    ResponseEntity<DiaryListResponse> getByUserId(@RequestHeader("Authorization") Long token) {
         List<Diary> diaryList = diaryService.getUserDiary(token);
 
         List<DiaryResponse> diaryResponseList = new ArrayList<>();
-        for(Diary diary : diaryList){
-            diaryResponseList.add(new DiaryResponse(diary.id(), diary.title() ,diary.date(), diary.category(), diary.isVisible(), diary.userId(), diary.nickname()));
+        for (Diary diary : diaryList) {
+            diaryResponseList.add(new DiaryResponse(diary.id(), diary.title(), diary.date(), diary.category(), diary.isVisible(), diary.userId(), diary.nickname()));
         }
 
         return ResponseEntity.ok(new DiaryListResponse(diaryResponseList));
     }
 
-    @PatchMapping("/api/diary/{id}")
-    void update(@PathVariable Long id, @RequestBody DiaryRequest diaryRequest, @RequestHeader("Authorization") Long token){
+    @PatchMapping("/{id}")
+    void update(@PathVariable Long id, @RequestBody DiaryRequest diaryRequest, @RequestHeader("Authorization") Long token) {
         diaryRequest.validate();
         diaryService.updateDiary(id, diaryRequest.title(), diaryRequest.content(), diaryRequest.category());
     }
 
-    @DeleteMapping("/api/diary/{id}")
-    void delete(@PathVariable Long id, @RequestHeader("Authorization") Long token){
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable Long id, @RequestHeader("Authorization") Long token) {
         diaryService.deleteDiary(id);
     }
 }
--- src/main/java/diary/controller/UserController.java
+++ src/main/java/diary/controller/UserController.java
@@ -4,11 +4,10 @@ import diary.dto.LoginRequest;
 import diary.dto.RegisterRequest;
 import diary.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/users")
 public class UserController {
 
     private final UserService userService;
@@ -17,13 +16,13 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping("/api/user/auth/login")
+    @PostMapping("/auth/login")
     ResponseEntity<Long> login(@RequestBody LoginRequest loginRequest) {
-       Long token = userService.login(loginRequest.username(), loginRequest.password());
-       return ResponseEntity.ok(token);
+        Long token = userService.login(loginRequest.username(), loginRequest.password());
+        return ResponseEntity.ok(token);
     }
 
-    @PostMapping("/api/user/auth/register")
+    @PostMapping("/auth/register")
     ResponseEntity<String> register(@RequestBody RegisterRequest registerRequest) {
         userService.register(registerRequest.username(), registerRequest.password(), registerRequest.nickname());
         return ResponseEntity.ok("회원가입이 완료되었습니다.");
